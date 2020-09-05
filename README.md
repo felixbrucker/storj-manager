945fe95157159685bf157cd307bc5807ddb4575b
Felix Brucker <felix@felixbrucker.com>
2020-09-05 09:35:14 +0200

    Init

---
+++ README.md
@@ -0,0 +1,13 @@
+Storj-Manager
+======
+
+[![Software License](https://img.shields.io/badge/license-GPL--3.0-brightgreen.svg?style=flat-square)](LICENSE)
+
+## Prerequisites
+
+- NodeJS
+- Windows
+
+## License
+
+GNU GPLv3 (see [LICENSE](https://github.com/felixbrucker/storj-manager/blob/master/LICENSE))
---
+++ main.js
@@ -0,0 +1,15 @@
+const binaryManager = require('./lib/services/binary-manager');
+const storageNodeManager = require('./lib/services/storage-node-manager');
+const config = require('./lib/services/config');
+
+(async () => {
+  try {
+    await config.init();
+    await binaryManager.init();
+    await storageNodeManager.init();
+  } catch (err) {
+    process.exit(1);
+  }
+
+  await storageNodeManager.start();
+})();
